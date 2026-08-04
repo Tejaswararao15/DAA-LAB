741781d5ad4769ee451efc8d14b4dd10f9d4a0be
Tejaswararao15 <tejaswararao.kadusu132539@marwadiuniversity.ac.in>
2026-08-05 00:25:34 +0800

    Refactor sorting algorithms to use vectors

--- Daa p1.cpp
+++ Daa p1.cpp
@@ -1,25 +1,30 @@
-
 #include <iostream>
-using namespace std;
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
 
-void bubbleSort(int a[], int n)
+using namespace std;
+using namespace std::chrono;
+
+void bubbleSort(vector<int> &a)
 {
+    int n = a.size();
+
     for(int i = 0; i < n - 1; i++)
     {
         for(int j = 0; j < n - i - 1; j++)
         {
             if(a[j] > a[j + 1])
-            {
-                int temp = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = temp;
-            }
+                swap(a[j], a[j + 1]);
         }
     }
 }
 
-void selectionSort(int a[], int n)
+void selectionSort(vector<int> &a)
 {
+    int n = a.size();
+
     for(int i = 0; i < n - 1; i++)
     {
         int min = i;
@@ -27,19 +32,17 @@ void selectionSort(int a[], int n)
         for(int j = i + 1; j < n; j++)
         {
             if(a[j] < a[min])
-            {
                 min = j;
-            }
         }
 
-        int temp = a[i];
-        a[i] = a[min];
-        a[min] = temp;
+        swap(a[i], a[min]);
     }
 }
 
-void insertionSort(int a[], int n)
+void insertionSort(vector<int> &a)
 {
+    int n = a.size();
+
     for(int i = 1; i < n; i++)
     {
         int key = a[i];
@@ -55,50 +58,32 @@ void insertionSort(int a[], int n)
     }
 }
 
-void merge(int a[], int left, int mid, int right)
+void merge(vector<int> &a, int left, int mid, int right)
 {
-    int temp[100];
+    vector<int> temp;
+
     int i = left;
     int j = mid + 1;
-    int k = 0;
 
     while(i <= mid && j <= right)
     {
         if(a[i] < a[j])
-        {
-            temp[k] = a[i];
-            i++;
-        }
+            temp.push_back(a[i++]);
         else
-        {
-            temp[k] = a[j];
-            j++;
-        }
-
-        k++;
+            temp.push_back(a[j++]);
     }
 
     while(i <= mid)
-    {
-        temp[k] = a[i];
-        i++;
-        k++;
-    }
+        temp.push_back(a[i++]);
 
     while(j <= right)
-    {
-        temp[k] = a[j];
-        j++;
-        k++;
-    }
+        temp.push_back(a[j++]);
 
-    for(i = left, k = 0; i <= right; i++, k++)
-    {
-        a[i] = temp[k];
-    }
+    for(int k = 0; k < temp.size(); k++)
+        a[left + k] = temp[k];
 }
 
-void mergeSort(int a[], int left, int right)
+void mergeSort(vector<int> &a, int left, int right)
 {
     if(left < right)
     {
@@ -111,7 +96,7 @@ void mergeSort(int a[], int left, int right)
     }
 }
 
-int partition(int a[], int low, int high)
+int partition(vector<int> &a, int low, int high)
 {
     int pivot = a[high];
     int i = low - 1;
@@ -121,21 +106,16 @@ int partition(int a[], int low, int high)
         if(a[j] < pivot)
         {
             i++;
-
-            int temp = a[i];
-            a[i] = a[j];
-            a[j] = temp;
+            swap(a[i], a[j]);
         }
     }
 
-    int temp = a[i + 1];
-    a[i + 1] = a[high];
-    a[high] = temp;
+    swap(a[i + 1], a[high]);
 
     return i + 1;
 }
 
-void quickSort(int a[], int low, int high)
+void quickSort(vector<int> &a, int low, int high)
 {
     if(low < high)
     {
@@ -146,60 +126,49 @@ void quickSort(int a[], int low, int high)
     }
 }
 
-void printArray(int a[], int n)
-{
-    for(int i = 0; i < n; i++)
-    {
-        cout << a[i] << " ";
-    }
-
-    cout << endl;
-}
-
 int main()
 {
-    int original[] = {64, 25, 12, 22, 11};
-    int n = 5;
+    int n = 100;
 
-    int a[100];
+    vector<int> arr(n);
+    vector<int> temp;
 
-    cout << "Original Array: ";
-    printArray(original, n);
-
-    for(int i = 0; i < n; i++)
-        a[i] = original[i];
-
-    bubbleSort(a, n);
-    cout << "Bubble Sort: ";
-    printArray(a, n);
+    srand(time(0));
 
     for(int i = 0; i < n; i++)
-        a[i] = original[i];
+        arr[i] = rand() % 1000;
 
-    selectionSort(a, n);
-    cout << "Selection Sort: ";
-    printArray(a, n);
+    cout << "Number of Elements = " << n << endl << endl;
 
-    for(int i = 0; i < n; i++)
-        a[i] = original[i];
+    temp = arr;
+    auto start = high_resolution_clock::now();
+    bubbleSort(temp);
+    auto stop = high_resolution_clock::now();
+    cout << "Bubble Sort Time    : " << duration_cast<microseconds>(stop - start).count() << " microseconds" << endl;
 
-    insertionSort(a, n);
-    cout << "Insertion Sort: ";
-    printArray(a, n);
+    temp = arr;
+    start = high_resolution_clock::now();
+    selectionSort(temp);
+    stop = high_resolution_clock::now();
+    cout << "Selection Sort Time : " << duration_cast<microseconds>(stop - start).count() << " microseconds" << endl;
 
-    for(int i = 0; i < n; i++)
-        a[i] = original[i];
+    temp = arr;
+    start = high_resolution_clock::now();
+    insertionSort(temp);
+    stop = high_resolution_clock::now();
+    cout << "Insertion Sort Time : " << duration_cast<microseconds>(stop - start).count() << " microseconds" << endl;
 
-    mergeSort(a, 0, n - 1);
-    cout << "Merge Sort: ";
-    printArray(a, n);
+    temp = arr;
+    start = high_resolution_clock::now();
+    mergeSort(temp, 0, n - 1);
+    stop = high_resolution_clock::now();
+    cout << "Merge Sort Time     : " << duration_cast<microseconds>(stop - start).count() << " microseconds" << endl;
 
-    for(int i = 0; i < n; i++)
-        a[i] = original[i];
-
-    quickSort(a, 0, n - 1);
-    cout << "Quick Sort: ";
-    printArray(a, n);
+    temp = arr;
+    start = high_resolution_clock::now();
+    quickSort(temp, 0, n - 1);
+    stop = high_resolution_clock::now();
+    cout << "Quick Sort Time     : " << duration_cast<microseconds>(stop - start).count() << " microseconds" << endl;
 
     return 0;
 }
